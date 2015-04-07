c79316c8c17c21f013f18a640c78dab3e99e399f
PeakPointer <zhang5474jj@163.com>
2015-04-07 12:45:31 +0800

    11

--- qq/comm/inc.php
+++ qq/comm/inc.php
@@ -1,2 +1,2 @@
 <?php die('forbidden'); ?>
-{"appid":"101180773","appkey":"c88f44f63ad1d14af1183017334860b8","callback":"http://www.vipmhxy.com/social/qq.html","scope":"get_user_info","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}
+{"appid":"101205181","appkey":"c99babfbe61f8e217d8a5f465fcfdfcb","callback":"http://www.vipzhangcheng.cn/social/qq.html","scope":"get_user_info","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}
