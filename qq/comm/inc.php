4c1b17bfa0714f925709d46777ea092916c62a9d
PeakPointer <zhang5474jj@163.com>
2015-04-27 16:34:43 +0800

    aa

--- qq/comm/inc.php
+++ qq/comm/inc.php
@@ -1,2 +1,2 @@
 <?php die('forbidden'); ?>
-{"appid":"101205181","appkey":"c99babfbe61f8e217d8a5f465fcfdfcb","callback":"http://www.vipzhangcheng.cn/social/qq.html","scope":"get_user_info","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}
+{"appid":"101205181","appkey":"c99babfbe61f8e217d8a5f465fcfdfcb","callback":"http://www.vipzhangcheng.cn/social/qq.html?","scope":"get_user_info","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}
