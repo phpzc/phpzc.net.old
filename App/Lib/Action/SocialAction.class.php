178f4c4368ef028abe42cb7f3328fb33d2701f91
zc <zhang5474jj@163.com>
2015-11-06 15:27:06 +0800

    ccc

--- App/Lib/Action/SocialAction.class.php
+++ App/Lib/Action/SocialAction.class.php
@@ -295,7 +295,7 @@ class SocialAction extends CommonAction {
 			$ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL, $url);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
+			curl_setopt($ch, CURLOPT_HEADER, 0);
 			$data = curl_exec($ch);
 			echo $data;
 			curl_close($ch);
