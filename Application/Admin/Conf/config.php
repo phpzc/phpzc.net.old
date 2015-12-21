a3ae08a50b0d354617383ff843e2dfd84fed6dce
PeakPointer <zhang5474jj@163.com>
2015-12-22 00:08:49 +0800

    1

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -6,7 +6,7 @@
  * Time: 0:14
  */
 return array(
-    //'DEFAULT_MODULE' => 'Admin',
+    'DEFAULT_MODULE' => 'Admin',
     'DEFAULT_M_LAYER'       =>  'Model', // 默认的模型层名称
     'DEFAULT_C_LAYER'       =>  'Controller', // 默认的控制器层名称
     'DEFAULT_V_LAYER'       =>  'View', // 默认的视图层名称
--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
     //'配置项'=>'配置值'
-    //'DEFAULT_MODULE'     => 'App', //默认模块
+    'DEFAULT_MODULE'     => 'App', //默认模块
     'DEFAULT_M_LAYER'       =>  'Model', // 默认的模型层名称
     'DEFAULT_C_LAYER'       =>  'Action', // 默认的控制器层名称
     'DEFAULT_V_LAYER'       =>  'Tpl', // 默认的视图层名称
