bcdd00a588c2eb1664679ed92c189d1eb65da5b7
zhangcheng <zhang5474jj@163.com>
2016-05-06 14:14:44 +0800

    add battle2

--- Application/App/Action/SocialAction.class.php
+++ Application/App/Action/SocialAction.class.php
@@ -378,6 +378,7 @@ class SocialAction extends CommonAction {
 		$data['redirect_uri']=urlencode(NET_NAME."/social/battle_callback");
 		$data['scope[]'] = "wow.profile";
 		$data['state'] = "zc";
+		$data['response_type'] = 'json';
 		foreach ($data as $key => $value) {
 			$url .= ($key."=".$value."&");
 		}
