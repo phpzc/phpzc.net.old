aecfbf765867e4bf6e7a4e0a3eac3999d3441cce
zhangcheng <zhang5474jj@163.com>
2016-05-13 10:02:28 +0800

    new admin model

---
+++ Laravel/laravel-5.1/app/Model/Index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zc
+ * Date: 2016/5/12
+ * Time: 9:52
+ */
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Index extends Model
+{
+    //
+    protected $table = 'index';
+
+    public $timestamps = false;
+    
+}
