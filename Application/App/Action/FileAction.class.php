e95e1c1008cff50a2daf189ced8825af34bbe6fe
zhangcheng.MacPro <zhang5474jj@163.com>
2017-01-04 23:16:24 +0800

    add file cater

---
+++ Application/App/Action/FileAction.class.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangcheng
+ * Date: 2017/1/4
+ * Time: 下午10:46
+ */
+
+namespace App\Action;
+
+
+class FileAction extends CommonAction
+{
+    public function index()
+    {
+
+    }
+
+
+    public function cat()
+    {
+        $file = I('get.file','');
+
+        $prefix = '/project/';
+
+        $filename = $prefix . $file;
+
+        if( file_exists($filename)){
+
+            $content = file_get_contents($filename);
+
+            $this->assign('title',$file);
+            $this->assign('content',$content);
+
+            $this->display();
+        }else{
+            $this->_empty();
+        }
+
+    }
+}
