cde5fddc095fed7724354b217afc8e5ecdbba15c
zhangcheng.MacPro <zhang5474jj@163.com>
2017-03-09 11:05:05 +0800

    create laravel 5.4 and vue.js for phpzc.net,for replace thinkphp code

---
+++ Laravel/laravel-5.4/app/Http/Controllers/Api/CommonController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangcheng
+ * Date: 2017/3/9
+ * Time: 上午10:41
+ */
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class CommonController extends Controller
+{
+
+}
---
+++ Laravel/laravel-5.4/app/Http/Controllers/Web/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangcheng
+ * Date: 2017/3/9
+ * Time: 上午10:43
+ */
+
+namespace App\Http\Controllers\Web;
+
+
+class IndexController extends CommonController
+{
+
+    public function index()
+    {
+        return 'index';
+    }
+}
