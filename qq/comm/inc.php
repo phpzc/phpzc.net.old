35572fddd785b831ad7fd48574ee6848d23e6765
zhangcheng.MacPro <zhang5474jj@163.com>
2017-03-28 16:47:56 +0800

    delete files

--- qq/comm/inc.php
+++ qq/comm/inc.php
@@ -1,2 +1,2 @@
 <?php die('forbidden'); ?>
-{"appid":"101331093","appkey":"e8affcbbf4e643c640a09fe9412f0450","callback":"https://www.phpzc.net/social/qq.html?","scope":"get_user_info","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}
+{"appid":"101331093","appkey":"e8affcbbf4e643c640a09fe9412f0450","callback":"https://www.phpzc.net/social/qq","scope":"get_user_info","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}
