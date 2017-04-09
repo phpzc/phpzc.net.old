f0b4cfefd146f1d300ac3bde8424ac7106d4403a
zhangcheng.MacPro <zhang5474jj@163.com>
2017-04-09 22:36:12 +0800

    add article name

--- Laravel/laravel-5.4/app/Http/Controllers/Web/ArticleController.php
+++ Laravel/laravel-5.4/app/Http/Controllers/Web/ArticleController.php
@@ -294,7 +294,7 @@ class ArticleController extends CommonController
         // 搜索文章
         $res = Article::where(['article.id'=>$id])
             ->join('user', 'article.uid', '=', 'user.id')
-            ->select('article.*')
+            ->select('article.*','user.name')
             ->first();
 
         if ($res) {
